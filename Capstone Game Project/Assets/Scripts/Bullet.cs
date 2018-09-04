dd66120f08b29e25572fb2857dabcc5b34a6afe7
HAL22 <thethelafaltein@gmail.com>
2018-09-04 19:54:34 +0200

    Initialized Tower AI

---
+++ Capstone Game Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Transform target;
+    private float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (speed<=0.0f || target == null)
+        {
+
+            Destroy(gameObject);
+
+        }
+
+        Vector3 direction = target.position - transform.position;
+
+        float distanceInFrame = speed * Time.deltaTime;
+
+        if (direction.magnitude <= distanceInFrame)
+        {
+            Destroy(gameObject);
+        }
+
+
+		
+	}
+
+    void SetData(Transform target, float speed)
+    {
+        this.target = target;
+        this.speed = speed;
+    }
+}
